658897fa96b3ad354b810e8a958c72bcb8fd4848
Zach Cervi <zachcervi@gmail.com>
2023-06-06 16:13:29 -0600

    Fonts

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,28 +1,28 @@
-import { View, Text, StyleSheet } from 'react-native'
-import Colors from '../../constants/colors'
-function NumberContainer ({ children }) {
+import { View, Text, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+function NumberContainer({ children }) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{children}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    alignItems: "center",
     borderColor: Colors.accent500,
     borderRadius: 8,
     borderWidth: 4,
-    justifyContent: 'center',
+    justifyContent: "center",
     margin: 24,
-    padding: 24
+    padding: 24,
   },
   text: {
     color: Colors.accent500,
     fontSize: 36,
-    fontWeight: 'bold'
-  }
-})
+    fontFamily: "open-sans-bold",
+  },
+});
 
-export default NumberContainer
+export default NumberContainer;
--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,19 +1,20 @@
-import { Text, StyleSheet } from 'react-native'
-import Colors from '../../constants/colors'
-function Title ({ children }) {
-  return <Text style={styles.title}>{children}</Text>
+import { Text, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
 }
 
-export default Title
+export default Title;
 
 const styles = StyleSheet.create({
   title: {
     borderColor: Colors.white,
     borderWidth: 2,
     color: Colors.white,
+    fontFamily: "open-sans-bold",
     fontSize: 24,
-    fontWeight: 'bold',
+
     padding: 12,
-    textAlign: 'center'
-  }
-})
+    textAlign: "center",
+  },
+});
