88c1479ec279554d363b99bba32a6e54dffc6d4a
Zach Cervi <zachcervi@gmail.com>
2023-06-06 11:24:08 -0600

    Main screen styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,46 +1,60 @@
-import { TextInput, View, StyleSheet } from "react-native";
-import PrimaryButton from "../components/PrimaryButton";
+import { TextInput, View, StyleSheet } from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
 
-function StartGameScreen() {
+function StartGameScreen () {
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
-        keyboardType="number-pad"
-        autoCapitalize="none"
+        keyboardType='number-pad'
+        autoCapitalize='none'
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    backgroundColor: "#72063c",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3b021f',
     borderRadius: 8,
     elevation: 4,
     marginHorizontal: 24,
     marginTop: 100,
     padding: 16,
-    shadowColor: "black",
+    shadowColor: 'black',
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.35,
-    shadowRadius: 6,
+    shadowRadius: 6
+  },
+  buttonsContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
   },
   numberInput: {
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: '#ddb52f',
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: '#ddb52f',
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     height: 50,
     width: 50,
     marginVertical: 8,
-    textAlign: "center",
-  },
-});
+    textAlign: 'center'
+  }
+})
 
-export default StartGameScreen;
+export default StartGameScreen
