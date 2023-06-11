0ff33285cd7b3faf425342d615089246a8aa394a
Zach Cervi <zachcervi@gmail.com>
2023-06-10 19:15:46 -0600

    Platform Specific code.

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 import Colors from "../../constants/colors";
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -8,8 +8,6 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
-    borderColor: Colors.white,
-    borderWidth: 2,
     color: Colors.white,
     fontFamily: "open-sans-bold",
     fontSize: 24,
